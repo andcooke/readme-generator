e359ac190087567101dfbef2a04c78abb2b15d02
A <andcooke@duck.com>
2022-07-17 15:49:19 -0500

    final README

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+
 const questions = [
   {
     type: 'input',
@@ -84,7 +85,7 @@ const writeREADME = (response) => {
   ${response.usage}
   
   ## License
-  
+
   This project is protected under the ${response.license}.
   
   ## Contributors
@@ -97,13 +98,12 @@ const writeREADME = (response) => {
   
   ## Questions
   
-  If you have any questions you may email me at ${response.email}.
+  If you have any questions or wish to contribute to this project you may email me at ${response.email}.
 
-  Or you may reach out to me at my GitHub profile here  [${response.name}](https://github.com/${response.github}).
+  Or you may reach out at my GitHub: [${response.name}](https://github.com/${response.github}).
   `;
 }
   
-
 
 
 // TODO: Create a function to initialize app
@@ -140,3 +140,21 @@ function getBadge (license) {
 
 // Function call to initialize app
 init();
+
+
+
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
